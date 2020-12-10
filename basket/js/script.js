e9c4c9bf630c34d1f470a223ebb374e9f0752db4
Eremet <eremetbekeshev@gmail.com>
2020-12-10 11:49:05 +0600

    sb bar

---
+++ basket/js/script.js
@@ -0,0 +1,17 @@
+var hideTest = document.getElementById('blankList')
+var hideList = document.getElementById('illustration')
+
+hideBlankList=()=>{
+    hideTest.style.display='none'
+}
+showList=()=>{
+    hideTest.style.display='flex'
+}
+
+hideList=()=>{
+    hideList.style.dysplay='none'
+}
+showIllustration=()=>{
+    hideTest.style.dysplay='none'
+    hideList.style.dysplay='flex'
+}
---
+++ secondBasket/js/script.js
@@ -0,0 +1,17 @@
+// var hideTest = document.getElementById('blankList')
+
+// hideBlankList=()=>{
+//     hideTest.style.display='none'
+// }
+// showList=()=>{
+//     hideTest.style.display='flex'
+// }
+
+// var hideList = document.getElementById('illustration')
+// hideList=()=>{
+//     hideList.style.dysplay='none'
+// }
+// showIllustration=()=>{
+//     hideTest.style.dysplay='none'
+//     hideList.style.dysplay='flex'
+// }
